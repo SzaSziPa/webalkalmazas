fde050a40f61080dc003a3f58290401ad6c21393
SzaSziPa <sz.szabina.kmne@gmail.com>
2020-04-17 19:28:43 +0200

    images

---
+++ script.js
@@ -0,0 +1,43 @@
+/*<div class="gallery_img-block">
+    <span class="">
+        Barlang Algarve környékén, Portugália
+    </span>
+    <img src="gallery/img_1" alt=""></img>
+</div>*/
+let gallery = [
+{
+    nev: 'Barlang Algarve környékén, Portugália',
+    leiras: 'img_1'
+}, 
+{
+    nev: 'Gásadalur, Feröer-szigetek',
+    leiras: 'img_2'
+},
+{
+    nev: 'Geiranger fjord, Norvégia',
+    leiras: 'img_3'
+},
+{
+    nev: 'Moreine-tó, Kanada',
+    leiras: 'img_4'
+},
+{
+    nev: 'Navagio-öböl, Görögország',
+    leiras: 'img_5'
+},
+{
+    nev: 'Plitvicei tavak, Horvátország',
+    leiras: 'img_6'
+},
+{
+    nev: 'Provence, Franciaország',
+    leiras: 'img_7'
+},
+{
+    nev: 'Vatnajokull gleccserbarlang, Izland',
+    leiras: 'img_8'
+},
+{
+    nev: 'Yosemite Nemzeti Park, Amerikai Egyesült Államok',
+    leiras: 'img_9'
+}];
